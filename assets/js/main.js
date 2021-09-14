2e66d4b0e78aa372e0970e8b6763ed0aef3d86ed
Shafiq Hammad <shafiqhammad5@gmail.com>
2021-09-14 20:42:09 +0600

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,46 @@
     }
   };
 
+  // ==== for menu scroll
+  const pageLink = document.querySelectorAll(".menu-scroll");
+
+  pageLink.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(elem.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+        offsetTop: 1 - 60,
+      });
+    });
+  });
+
+  // section menu active
+  function onScroll(event) {
+    const sections = document.querySelectorAll(".menu-scroll");
+    const scrollPos =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
+
+    for (let i = 0; i < sections.length; i++) {
+      const currLink = sections[i];
+      const val = currLink.getAttribute("href");
+      const refElement = document.querySelector(val);
+      const scrollTopMinus = scrollPos + 73;
+      if (
+        refElement.offsetTop <= scrollTopMinus &&
+        refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+      ) {
+        document.querySelector(".menu-scroll").classList.remove("active");
+        currLink.classList.add("active");
+      } else {
+        currLink.classList.remove("active");
+      }
+    }
+  }
+
+  window.document.addEventListener("scroll", onScroll);
+
   //===== close navbar-collapse when a  clicked
   let navbarToggler = document.querySelector(".navbar-toggler");
   const navbarCollapse = document.querySelector(".navbar-collapse");
@@ -57,6 +97,15 @@
 
   // ===== wow js
   new WOW().init();
+
+  // ========= glightbox
+  const myGallery = GLightbox({
+    href: "https://www.youtube.com/watch?v=r44RKWyfcFw",
+    type: "video",
+    source: "youtube", //vimeo, youtube or local
+    width: 900,
+    autoplayVideos: true,
+  });
 
   // ====== scroll top js
   function scrollTo(element, to = 0, duration = 500) {
